8969a7efea839c9af06cb54cfbf7c8590f79aeb9
Gabriel <gabrielschug@hotmail.com>
2025-11-05 01:02:36 -0300

    ORGANIZADOS todos arquivos da pasta SRC

--- src/components/Label.jsx
+++
@@ -1,8 +0,0 @@
-export default function Label({ label_for, texto }) {
-
-  return (
-    <>
-      <label for={label_for} className="text-sm/8 font-medium text-gray-900">{texto}</label>
-    </>
-  )
-}
--- src/components/Subtitulo.jsx
+++
@@ -1,8 +0,0 @@
-export default function Subtitulo({ texto }) {
-
-  return (
-    <>
-      <h2 className="px-4 py-2 text-lg text-textop" >{texto}</h2>
-    </>
-  )
-}
